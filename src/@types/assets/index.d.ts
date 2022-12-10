12db6ecdb9209a6dc29ad1f553e26463024ef5b4
David Wheatley <david@davwheat.dev>
2022-12-10 11:23:17 +0000

    chore: format

--- src/@types/assets/index.d.ts
+++ src/@types/assets/index.d.ts
@@ -1,36 +1,36 @@
 declare module '*.png' {
-  const value: string
-  export default value
+  const value: string;
+  export default value;
 }
 
 declare module '*.jpg' {
-  const value: string
-  export default value
+  const value: string;
+  export default value;
 }
 
 declare module '*.inline.svg' {
-  import { ReactElement, SVGProps } from 'react'
+  import { ReactElement, SVGProps } from 'react';
 
-  const content: (props: SVGProps<SVGElement>) => ReactElement
-  export default content
+  const content: (props: SVGProps<SVGElement>) => ReactElement;
+  export default content;
 }
 
 declare module '*.svg' {
-  const value: string
-  export default value
+  const value: string;
+  export default value;
 }
 
 declare module '*.mp3' {
-  const value: string
-  export default value
+  const value: string;
+  export default value;
 }
 
 declare module '*.ogg' {
-  const value: string
-  export default value
+  const value: string;
+  export default value;
 }
 
 declare module '*.woff2' {
-  const value: string
-  export default value
+  const value: string;
+  export default value;
 }
--- src/@types/global/index.d.ts
+++ src/@types/global/index.d.ts
@@ -1,11 +1,11 @@
-import type * as Leaflet from 'leaflet'
+import type * as Leaflet from 'leaflet';
 
-declare type ValuesOf<T extends readonly unknown[]> = T[number]
+declare type ValuesOf<T extends readonly unknown[]> = T[number];
 
 // declare var globalVar: mytype
 
 declare global {
   interface Window {
-    L: typeof Leaflet
+    L: typeof Leaflet;
   }
 }
