e3380a7cb8a9cb1283b2a656cea264992bcb0d1e
David Wheatley <david@davwheat.dev>
2022-12-27 23:54:09 +0100

    feat: add breadcrumbs to admin modem list

--- src/pages/admin/modems/index.tsx
+++ src/pages/admin/modems/index.tsx
@@ -3,6 +3,7 @@ import Layout from '@components/Design/Layout';
 import Hero from '@components/Design/Hero';
 import Section from '@components/Design/Section';
 import AdminModemsList from '@components/Admin/AdminModemsList';
+import Breadcrumbs from '@components/Design/Breadcrumbs';
 
 import type { HeadFC, PageProps } from 'gatsby';
 
@@ -12,6 +13,23 @@ export default function AdminModemsPage({ location }: PageProps) {
       <Hero firstElement>
         <h1 className="text-shout">Admin &mdash; Modems</h1>
       </Hero>
+
+      <Breadcrumbs
+        data={[
+          {
+            t: 'Home',
+            url: `/`,
+          },
+          {
+            t: 'Admin',
+            url: `/admin`,
+          },
+          {
+            t: 'Manage modems',
+            url: `/admin/modems`,
+          },
+        ]}
+      />
 
       <Section>
         <AdminModemsList />
