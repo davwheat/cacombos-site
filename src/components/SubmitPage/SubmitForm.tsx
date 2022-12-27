03a4ba4928f5c7c703a2bbee75ef2d85aa6d0d7a
David Wheatley <david@davwheat.dev>
2022-12-27 17:52:17 +0100

    feat: add contribution page

---
+++ src/components/SubmitPage/SubmitForm.tsx
@@ -0,0 +1,199 @@
+import MinorAlert from '@components/Design/MinorAlert';
+import Button from '@components/Inputs/Button';
+import Checkbox from '@components/Inputs/Checkbox';
+import FileInput from '@components/Inputs/FileInput';
+import TextArea from '@components/Inputs/TextArea';
+import TextBox from '@components/Inputs/TextBox';
+import Link from '@components/Links/Link';
+import { useSnackbar } from 'notistack';
+import React, { useCallback, useState } from 'react';
+
+interface SubmitFormState {
+  email: string;
+  deviceName: string;
+  deviceModel: string;
+  deviceFirmware: string;
+  comment: string;
+  log: File | null;
+  privacyAgree: boolean;
+}
+
+export default function SubmitForm() {
+  const [formState, setFormState] = useState<SubmitFormState>({
+    email: '',
+    deviceName: '',
+    deviceModel: '',
+    deviceFirmware: '',
+    comment: '',
+    log: null,
+    privacyAgree: false,
+  });
+  const [formError, setFormError] = useState<React.ReactNode>(null);
+  const [formSubmitting, setFormSubmitting] = useState<boolean>(false);
+
+  const { enqueueSnackbar } = useSnackbar();
+
+  const showError = useCallback(
+    (message: string) => {
+      enqueueSnackbar(message, { variant: 'error' });
+      setFormError(message);
+    },
+    [enqueueSnackbar, setFormError]
+  );
+
+  const hideError = useCallback(() => {
+    setFormError(null);
+  }, [setFormError]);
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+
+        if (!formState.privacyAgree) {
+          showError('You must agree to the privacy policy to submit data.');
+          return;
+        }
+
+        if (!formState.log && !formState.comment.trim()) {
+          showError('You must provide a log file or comment.');
+          return;
+        }
+
+        // Validation complete
+
+        hideError();
+        setFormSubmitting(true);
+
+        const formData = new FormData();
+        formData.append('fromUser', formState.email);
+        formData.append('deviceName', formState.deviceName);
+        formData.append('deviceModel', formState.deviceModel);
+        formData.append('deviceFirmware', formState.deviceFirmware);
+        formData.append('comment', formState.comment);
+        if (formState.log) formData.append('log', formState.log);
+      }}
+      css={{
+        marginTop: 24,
+      }}
+    >
+      <div
+        css={{
+          display: 'grid',
+          gridTemplateColumns: '1fr 1fr',
+          gridTemplateAreas: `
+          'email email'
+          'deviceName deviceModel'
+          'deviceFirmware deviceFirmware'
+          'log log'
+          'comment comment'`,
+          gap: 16,
+        }}
+      >
+        <TextBox
+          label="Your email (optional)"
+          helpText="This will be used to contact you in relation to your submission"
+          value={formState.email}
+          disabled={formSubmitting}
+          onInput={(val) => setFormState({ ...formState, email: val })}
+          css={{ gridArea: 'email' }}
+        />
+
+        <TextBox
+          label="Device name"
+          helpText="The name of your device (e.g., Google Pixel 7 Pro)"
+          value={formState.deviceName}
+          disabled={formSubmitting}
+          onInput={(val) => setFormState((s) => ({ ...s, deviceName: val }))}
+          css={{ gridArea: 'deviceName' }}
+        />
+
+        <TextBox
+          label="Device model"
+          helpText="Your device's model number. Some devices have regional variants, so please submit the correct model."
+          value={formState.deviceModel}
+          disabled={formSubmitting}
+          onInput={(val) => setFormState((s) => ({ ...s, deviceModel: val }))}
+          css={{ gridArea: 'deviceModel' }}
+        />
+
+        <TextBox
+          label="Device firmware"
+          helpText="The firmware version of your device (e.g., QPR Beta 1)"
+          value={formState.deviceFirmware}
+          disabled={formSubmitting}
+          onInput={(val) => setFormState((s) => ({ ...s, deviceFirmware: val }))}
+          css={{ gridArea: 'deviceFirmware' }}
+        />
+
+        <div css={{ gridArea: 'log' }}>
+          <p className="text-speak-up">Device log</p>
+          <FileInput
+            label="Upload your device log"
+            disabled={formSubmitting}
+            onInput={(val) => {
+              if ((val?.size ?? 0) > 1024 * 1024 * 25) {
+                showError('The log file must be smaller than 25 MB.');
+                setFormState((s) => ({ ...s, log: null }));
+                return;
+              }
+
+              setFormState((s) => ({ ...s, log: val }));
+            }}
+          />
+          <p className="text-whisper" css={{ marginTop: 8 }}>
+            Maximum file size is 25 MB. If your file is larger than this, please use a file transfer service, like Google Drive,{' '}
+            <Link href="https://wetransfer.com/">WeTransfer</Link> or <Link href="https://www.dropbox.com/transfer">Dropbox Transfer</Link> and place
+            the link in the box below.
+          </p>
+        </div>
+
+        <TextArea
+          label="Additional info"
+          helpText="Any additional information you'd like to provide (e.g., the device's carrier, any modifications made, any band locking, etc.)"
+          value={formState.comment}
+          disabled={formSubmitting}
+          onInput={(val) => setFormState((s) => ({ ...s, comment: val }))}
+          css={{ gridArea: 'comment' }}
+        />
+      </div>
+
+      <MinorAlert color="primaryBlue" coloredBackground css={{ marginTop: 32 }} heading="Privacy information">
+        <p className="text-speak">
+          Some device logs may contain sensitive information, such as device information, IMEIs, SIM ICCIDs, phone numbers, location, and more.
+        </p>
+        <p className="text-speak">
+          When you submit your device log to this form, it will be sent securely to our site administrator. No logs are kept for longer than is needed
+          to process them, and are deleted after they are processed and uploaded (or deemed unusable).
+        </p>
+      </MinorAlert>
+
+      <Checkbox
+        label="I agree to the privacy information above"
+        disabled={formSubmitting}
+        css={{ marginTop: 16 }}
+        checked={formState.privacyAgree}
+        onChange={() => {
+          setFormState((s) => ({ ...s, privacyAgree: !s.privacyAgree }));
+        }}
+      />
+
+      {formError && (
+        <MinorAlert
+          coloredBackground
+          color="primaryRed"
+          heading="Error"
+          css={{
+            marginTop: 16,
+          }}
+        >
+          <p className="text-speak">{formError}</p>
+        </MinorAlert>
+      )}
+
+      <Button type="submit" css={{ marginTop: 24 }} loading={formSubmitting}>
+        Submit form
+      </Button>
+    </form>
+  );
+}
---
+++ src/pages/contribute-combos.tsx
@@ -0,0 +1,169 @@
+import { SEO } from '@components/SEO';
+import Layout from '@components/Design/Layout';
+import Section from '@components/Design/Section';
+import Hero from '@components/Design/Hero';
+import MinorAlert from '@components/Design/MinorAlert';
+import Colors from '@data/colors.json';
+import Accordion from '@components/Design/Accordion';
+import Link from '@components/Links/Link';
+import CodeBlock from '@components/CodeBlock';
+import TextBox from '@components/Inputs/TextBox';
+import SubmitForm from '@components/SubmitPage/SubmitForm';
+import OpenInNewIcon from 'mdi-react/OpenInNewIcon';
+
+import type { HeadFC, PageProps } from 'gatsby';
+
+export default function SubmitPage({ location }: PageProps) {
+  return (
+    <Layout location={location}>
+      <Hero color={Colors.primaryRed} firstElement>
+        <h1 className="text-shout">Submit combos</h1>
+      </Hero>
+
+      <Section>
+        <h2 className="text-louder">Getting combos from your device</h2>
+
+        <p className="text-speak">
+          There are various ways to get the required carrier combinations from your device. Information for many mobile phones is available below.
+        </p>
+
+        <MinorAlert coloredBackground color="cautioningAmber" heading="Warning">
+          <p className="text-speak">
+            We <strong>cannot</strong> accept screenshots of applications like NetMonster, Cellmapper or NSG to demonstrate combos.
+          </p>
+        </MinorAlert>
+
+        <div css={{ marginTop: 16 }}>
+          <Accordion heading="Qualcomm diag (root)">
+            <MinorAlert coloredBackground color="primaryBlue" heading="Requirements" css={{ marginBottom: 16 }}>
+              <p className="text-speak">This method should work on any rooted Qualcomm Android device.</p>
+            </MinorAlert>
+
+            <ol className="list">
+              <li>
+                <p className="text-speak">
+                  Download and extract <Link href="https://developer.android.com/studio/releases/platform-tools">Android Platform Tools</Link> to your
+                  device. On Linux, this might be available through your package manager.
+                </p>
+              </li>
+              <li>
+                <p className="text-speak">Connect your phone to your computer via USB, and enable ADB debugging in Developer Options.</p>
+              </li>
+              <li>
+                <p className="text-speak">
+                  Launch your terminal (e.g., PowerShell on Windows) and navigate to the directory where you extracted the platform tools.
+                </p>
+              </li>
+              <li>
+                <p className="text-speak">
+                  Run <code>adb shell</code> in your terminal to launch a remote shell on your Android device
+                </p>
+              </li>
+              <li>
+                <p className="text-speak">Run the following commands in your device shell:</p>
+                <CodeBlock>
+                  {() => `cd /sdcard
+mkdir -p /data/vendor/ramdump/diag_logs/
+mkdir -p diag_logs
+cd diag_logs
+curl https://mobilecombos.com/assets/diag.zip > diag.zip
+unzip diag.zip`}
+                </CodeBlock>
+              </li>
+              <li>
+                <p className="text-speak">
+                  Next, use the <code className="code">su</code> command to gain root privileges and then begin logging:
+                </p>
+                <CodeBlock>
+                  {() => `su
+diag_mdlog -o /sdcard/diag_logs -f /sdcard/diag_logs/Diag.cfg -m /sdcard/diag_logs/Diag.cfg`}
+                </CodeBlock>
+              </li>
+              <li>
+                When logging, turn Airplane Mode on, wait 5 seconds, then turn it back off. Wait 2 mins to reconnect to your network. After this, end
+                the logging session with <kbd>Ctrl + C</kbd>. Your log file will be saved in <code className="code">/sdcard/diag_log</code>. This is
+                the file you need to send.
+              </li>
+            </ol>
+          </Accordion>
+
+          <Accordion heading="Network Signal Guru premium (root and paid)">
+            <MinorAlert coloredBackground color="primaryBlue" heading="Requirements" css={{ marginBottom: 16 }}>
+              <p className="text-speak">Requires a paid NSG license and a rooted Android device.</p>
+            </MinorAlert>
+
+            <ol className="list">
+              <li>
+                <p className="text-speak">Open Network Signal Guru.</p>
+              </li>
+              <li>
+                <p className="text-speak">Swipe to the Signalling page.</p>
+              </li>
+              <li>
+                <p className="text-speak">
+                  Toggle Airplane Mode on, wait 5 seconds, then turn it off and wait for your device to connect to the network.
+                </p>
+              </li>
+              <li>
+                <p className="text-speak">
+                  Find a signalling message named "UE Capability Information" (see screenshot below). You may see more than one on a 5G device. If you
+                  cannot find it, try toggling airplane mode again.
+                </p>
+              </li>
+              <li>
+                <p className="text-speak">
+                  Save the log (press <OpenInNewIcon className="icon" /> in the top-right) and upload it to this form.
+                </p>
+              </li>
+            </ol>
+          </Accordion>
+
+          <Accordion heading="Network Signal Guru free (root)">
+            <MinorAlert coloredBackground color="primaryBlue" heading="Requirements" css={{ marginBottom: 16 }}>
+              <p className="text-speak">Requires a rooted Android device. This method does not always work!</p>
+            </MinorAlert>
+
+            <ol className="list">
+              <li>
+                <p className="text-speak">Open Network Signal Guru.</p>
+              </li>
+              <li>
+                <p className="text-speak">
+                  Toggle Airplane Mode on, wait 5 seconds, then turn it off and wait for your device to connect to the network.
+                </p>
+              </li>
+              <li>
+                <p className="text-speak">Wait approximately 30 seconds, then close NSG.</p>
+              </li>
+              <li>
+                <p className="text-speak">
+                  Upload the file <code className="code">/sdcard/NSG/test.log</code> to this form.
+                </p>
+              </li>
+            </ol>
+          </Accordion>
+        </div>
+
+        <p className="text-speak" css={{ marginTop: 16 }}>
+          For more logging methods, please check{' '}
+          <Link href="https://cacombos.com/contribute" target="_blank">
+            CA Combos' contribution page
+          </Link>
+          . Most formats supported by CA Combos are also supported by my site.
+        </p>
+      </Section>
+      <Section darker>
+        <h2 className="text-louder">Submit combo info for your device</h2>
+
+        <p className="text-speak">
+          Use the form below to submit information about your device to our website. If your submission is not complete, your combos may not be
+          published.
+        </p>
+
+        <SubmitForm />
+      </Section>
+    </Layout>
+  );
+}
+
+export const Head: HeadFC = () => <SEO pageName="Contribute"></SEO>;
