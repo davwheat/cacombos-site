628258999c119e285ceb0b7a19a70f426c6ebeff
David Wheatley <david@davwheat.dev>
2023-05-23 10:23:14 +0100

    fix: page functions erroring

--- functions/admin/devices/edit/[[all]].ts
+++ functions/admin/devices/edit/[[all]].ts
@@ -3,7 +3,7 @@ import { type EventContext, type Fetcher } from '@cloudflare/workers-types';
 const NEW_URI = '/admin/modems/edit/[...]/';
 
 export function onRequestGet({ env, request }: EventContext<Env, string, unknown>) {
-  return env.ASSETS.fetch(new URL(NEW_URI, request.url), request).toString();
+  return env.ASSETS.fetch(new Request(new URL(NEW_URI, request.url).toString(), request));
 }
 
 interface Env {
